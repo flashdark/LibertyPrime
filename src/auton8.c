7c1ed367c76f0314d81eaed006ff9b4d6dead231
gnations56 <gnationsonyoutube@outlook.com>
2018-01-09 16:20:45 -0700

    fixed braking algorithm and added go straight code for auton

--- src/auton8.c
+++ src/auton8.c
@@ -11,32 +11,77 @@ static int mp = 0;
 void auton8()
 {
   static int maxspeed = 0;
-
-    motorLeftDriveSet(50);
-    motorRightDriveSet(50);
+  mp = 50;
+    motorLeftDriveSet(mp);
+    motorRightDriveSet(mp);
     while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(DIST)-400) )
     {
         if (speed > maxspeed)
         {
           maxspeed = speed;
-
         }
+
+        static int offset = 0;
+        if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) >= 15  )
+        {
+          offset += 10;
+        }
+
+        else if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) >= 10  )
+        {
+          offset += 5;
+        }
+
+        else if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) >= 5  )
+        {
+          offset += 2;
+        }
+
+        else if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) <= -5  )
+        {
+          offset -= 2;
+        }
+
+        else if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) <= -10  )
+        {
+          offset -= 5;
+        }
+        
+        else if ( (encoderGet(RightDriveEncoder) - encoderGet(LeftDriveEncoder)) <= -15  )
+        {
+          offset -= 10;
+        }
+
+
+        else if ( (encoderGet(LeftDriveEncoder) >= -5) && (encoderGet(LeftDriveEncoder) <= 5) )
+        {
+          offset = 0;
+        }
+
+        if ( (offset > 500))
+        {
+          offset = 500;
+        }
+        else if (offset < -500)
+        {
+          offset = -500;
+        }
+
+      //motorLeftDriveSet(mp+offset);
+      motorRightDriveSet(mp-offset/50);
+
     }
 
     while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(DIST)-375) )
     {
-
       //speed =
       if (speed > OK_TO_BRAKE)
       {
         //char buf[17];
         mp = -2 * (speed / 4.5);
       }
-
-
       motorLeftDriveSet(mp);
       motorRightDriveSet(mp);
-
     }
 
     mp = 20;
@@ -46,5 +91,4 @@ void auton8()
     while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(DIST)) );
     motorLeftDriveSet(0);
     motorRightDriveSet(0);
-
 }
