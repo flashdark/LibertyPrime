287fd00613d058e2ef6d5c0908a36d796f957b4a
gnations56 <gnationsonyoutube@outlook.com>
2018-01-25 06:09:27 -0700

    added comments and removed partial auton3. auton3 is ready for reuse

--- src/auton2.c
+++ src/auton2.c
@@ -3,70 +3,71 @@
 #define DIST 36
 void auton2()
 {
-  motorSet(CLAW_MOTOR,-100);
+
+  motorSet(CLAW_MOTOR,-100);//pull cone in
   delay(250);
-  motorSet(CLAW_MOTOR,-10);
-  motorSet(LIFT_MOTOR,100);
+  motorSet(CLAW_MOTOR,-10);//hold cone
+  motorSet(LIFT_MOTOR,100);//lift up
   while( encoderGet(LiftEncoder) < 17)
   {
 
   }
   motorSet(LIFT_MOTOR,0);
-  motorSet(MOBILE_GOAL_MOTOR,-100);
-  motorLeftDriveSet(50);
-  motorRightDriveSet(50);
+  motorSet(MOBILE_GOAL_MOTOR,-100);//deploy mobile goal
+  motorLeftDriveSet(50);//drive forward
+  motorRightDriveSet(50);//drive forward
   while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(51) )  )
   {
 
   }
-  motorLeftDriveSet(0);
-  motorRightDriveSet(0);
+  motorLeftDriveSet(0);//stop
+  motorRightDriveSet(0);//stop
   motorSet(MOBILE_GOAL_MOTOR,100);
   while(analogRead(MOBILE_GOAL_POT) > 10)
   {
 
   }
-  motorSet(MOBILE_GOAL_MOTOR,0);
+  motorSet(MOBILE_GOAL_MOTOR,0);//stop pulling in
   motorSet(ARM_MOTOR,-25);
   while (encoderGet(ArmEncoder) < 64){}
   motorSet(ARM_MOTOR,0);
   delay(100);
-  motorSet(LIFT_MOTOR,-50);
+  motorSet(LIFT_MOTOR,-50);//lower lift
   while( encoderGet(LiftEncoder) > 7)
   {
 
   }
 
-  motorSet(CLAW_MOTOR,60);
+  motorSet(CLAW_MOTOR,60);//drop cone
   delay(100);
-  motorSet(LIFT_MOTOR,50);
+  motorSet(LIFT_MOTOR,50);//lift up again
   while( encoderGet(LiftEncoder) > 17)
   {
 
   }
   delay(100);
-motorSet(CLAW_MOTOR,0);
-motorSet(LIFT_MOTOR,0);
+motorSet(CLAW_MOTOR,0);//stop releasing cone
+motorSet(LIFT_MOTOR,0);//stop lifting
 delay(100);
-motorLeftDriveSet(-100);
-motorRightDriveSet(-100);
+motorLeftDriveSet(-100);//drive backwards
+motorRightDriveSet(-100);//drive backwards
 while( (encoderGet(LeftDriveEncoder) > encoderInchesToCounts(28) )  )
 {
 
 }
-motorLeftDriveSet(0);
-motorRightDriveSet(0);
+motorLeftDriveSet(0);//stop driving
+motorRightDriveSet(0);//stop driving
 delay(250);
-encoderReset(LeftDriveEncoder);
+encoderReset(LeftDriveEncoder);//reset encoder
 delay(100);
-motorLeftDriveSet(-50);
-motorRightDriveSet(50);
+motorLeftDriveSet(-50);//turn left
+motorRightDriveSet(50);//turn left
 while(encoderGet(LeftDriveEncoder) >= encoderInchesToCounts(-15))
 {
 
 }
-motorLeftDriveSet(0);
-motorRightDriveSet(0);
+motorLeftDriveSet(0);//stop driving
+motorRightDriveSet(0);//stop driving
 delay(100);
 // motorLeftDriveSet(-50);
 // motorRightDriveSet(-50);
--- src/auton5.c
+++ src/auton5.c
@@ -1,54 +1,56 @@
 #include <API.H>
 #include "robot.h"
-
+//////////////////////////
+//Stationary goal auton//
+////////////////////////
 void auton5()
 {
-  motorSet(CLAW_MOTOR,-100);
+  motorSet(CLAW_MOTOR,-100);//pull in cone
   delay(250);
-  motorSet(CLAW_MOTOR,0);
-  motorLeftDriveSet(50);
-  motorRightDriveSet(50);
-  while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(15.5) )  )
+  motorSet(CLAW_MOTOR,-10);//apply hold power
+  motorLeftDriveSet(50);//drive forward at half power
+  motorRightDriveSet(50);//drive forward at half power
+  while( (encoderGet(LeftDriveEncoder) < encoderInchesToCounts(15.5) )  )//drive to stationary goal
   {
 
   }
-  motorLeftDriveSet(0);
-  motorRightDriveSet(0);
+  motorLeftDriveSet(0);//stop driving
+  motorRightDriveSet(0);//stop driving
   delay(100);
-  motorSet(LIFT_MOTOR,100);
-  while( encoderGet(LiftEncoder) < 37)
+  motorSet(LIFT_MOTOR,100);//start lifting
+  while( encoderGet(LiftEncoder) < 37)//lift up to stationary goal
   {
 
   }
-  motorSet(LIFT_MOTOR,20);
+  motorSet(LIFT_MOTOR,20);//apply hold power to stabilize lift
   delay(300);
-  motorSet(ARM_MOTOR,-25);
-  while (encoderGet(ArmEncoder) < 86){}
-  motorSet(ARM_MOTOR,0);
+  motorSet(ARM_MOTOR,-25);//start deploying arm
+  while (encoderGet(ArmEncoder) < 86){}//deploy arm
+  motorSet(ARM_MOTOR,0);//stop deploying arm
   delay(100);
-  motorSet(LIFT_MOTOR,-50);
+  motorSet(LIFT_MOTOR,-50);//lower cone onto stationary goal
   while( encoderGet(LiftEncoder) > 27)
   {
 
   }
-  motorSet(LIFT_MOTOR,0);
-  motorSet(CLAW_MOTOR,0);
+  motorSet(LIFT_MOTOR,0);//stop moving lif down
+  motorSet(CLAW_MOTOR,0);//release intake power
   delay(100);
-  motorSet(CLAW_MOTOR,60);
-  motorSet(LIFT_MOTOR,75);
+  motorSet(CLAW_MOTOR,60);//drop cone
+  motorSet(LIFT_MOTOR,75);//lift up to finish releasing the cone
   while( encoderGet(LiftEncoder) < 37)
   {
 
   }
-  motorSet(LIFT_MOTOR,0);
-  motorLeftDriveSet(-50);
+  motorSet(LIFT_MOTOR,0);//stop lifting up
+  motorLeftDriveSet(-50);//back away from goal
   motorRightDriveSet(-50);
   motorSet(CLAW_MOTOR,0);
   while( (encoderGet(LeftDriveEncoder) > encoderInchesToCounts(11)) )
   {
 
   }
-  motorLeftDriveSet(0);
+  motorLeftDriveSet(0);//stop driving
   motorRightDriveSet(0);
 
 
